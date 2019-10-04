2901b01e06cc96e7e456e89e85c530c57b8cb216
Almir Zulic <almir@bind.ba>
2019-10-04 12:38:19 +0200

    fix

--- js/script.js
+++ js/script.js
@@ -1,38 +1,44 @@
 function validNumber() {
   let binary = document.getElementById('number').value;
-  document.getElementById('number').value = "";
   for (i = 0; i < binary.length; i++) {
     const inputNumber = binary[i];
     if(inputNumber < 0 || inputNumber > 1) {
       document.getElementById('invalid').innerText = 'NUMBER IS NOT BINARY' 
+      return false;
     }
-    return true;
   }
+  return true;
+
 }
 
 function pow(base, exp) {
-  let binary = document.getElementById('number').value;
   if (exp == 0) return 1;
   if (exp == 1) return base;
   let result = 0;
-  for (i = exp; exp > 0; i--){
+  for (let i = exp; i > 0; i--){
     if (result == 0) {
-      return base;
+      result = base;
     } else {
       result = result * base;
     }
-    return result;
   }
+  return result;
+}
+
+function convert() {
+  let binary = document.getElementById('number').value;
   let convertNumber = 0;
   for(let i = 0; i < binary.length; i++) {
     convertNumber += binary[i] * pow(2, i);
-    document.getElementById('result').innerText = convertNumber;
   }
+  return convertNumber;
 }
 
 function converted () {
-  validNumber();
-  pow();
+  if(!validNumber()) {
+    return;
+  };
+  console.log(convert())
 }
 
 
