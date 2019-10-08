9e1342bbe8db8f4df3de113e0119e4e508c6e385
Elvir Uzunovic <uzunovicelvir@gmail.com>
2019-10-08 20:58:33 +0200

    always return 0

--- js/script.js
+++ js/script.js
@@ -1,38 +1,46 @@
-function validNumber() {
+
+function validNumber () {
   let binary = document.getElementById('number').value;
-  document.getElementById('number').value = "";
-  for (i = 0; i < binary.length; i++) {
+  document.getElementById('number').value ="";
+  for(let i = 0; i < binary.length; i++) {
     const inputNumber = binary[i];
-    if(inputNumber < 0 || inputNumber > 1) {
+    if(inputNumber < 0 || inputNumber > 1){
       document.getElementById('invalid').innerText = 'NUMBER IS NOT BINARY' 
+      return false
     }
-    return true;
-  }
+  } 
+  document.getElementById('invalid').style = 'display:none';
+  return true
+  
 }
 
-function pow(base, exp) {
-  let binary = document.getElementById('number').value;
+function pow(base,exp) {
   if (exp == 0) return 1;
   if (exp == 1) return base;
   let result = 0;
-  for (i = exp; exp > 0; i--){
-    if (result == 0) {
-      return base;
+  for(let i = exp; i > 0; i--){
+    if(result == 0){
+      result = base;
     } else {
       result = result * base;
     }
-    return result;
   }
-  let convertNumber = 0;
-  for(let i = 0; i < binary.length; i++) {
-    convertNumber += binary[i] * pow(2, i);
-    document.getElementById('result').innerText = convertNumber;
+  return result;
+}
+
+function convert () {
+  let binary = document.getElementById('number').value;
+  let number = ``;
+  for (let i = 0; i < binary.length - 1; i++){
+    number += binary[i] * pow(2, i);
   }
+  document.getElementById('result').innerText = number;
+  return number;
 }
 
 function converted () {
-  validNumber();
-  pow();
+  if(!validNumber()){
+    return
+  }
+  console.log(convert());
 }
-
-
